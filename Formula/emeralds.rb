5a976e46ff7f1a09a2d4e3e64250627650aa30c8
atha <atha@fourth-wall.org>
2026-05-24 02:05:20 +0300

    Create emeralds.rb

---
+++ Formula/emeralds.rb
@@ -0,0 +1,19 @@
+class Emeralds < Formula
+  desc "A package manager for C"
+  homepage "https://github.com/Oblivious-Oblivious/Emeralds"
+  url "https://github.com/Oblivious-Oblivious/Emeralds/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  license "MIT"
+
+  depends_on "crystal" => :build
+
+  def install
+    system "shards", "build", "--release", "--no-debug"
+    bin.install "bin/emeralds"
+    bin.install_symlink bin/"emeralds" => "em"
+  end
+
+  test do
+    assert_match "init", shell_output("#{bin}/em help")
+  end
+end
