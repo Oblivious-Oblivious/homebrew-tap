29c00eddea5587f0b100f3e34e12cecd0e6267c1
atha <atha@fourth-wall.org>
2026-05-24 13:28:10 +0300

    [master] - updated emeralds to 0.10.11

--- emeralds.rb
+++ emeralds.rb
@@ -1,8 +1,8 @@
 class Emeralds < Formula
   desc "A package manager for C"
   homepage "https://github.com/Oblivious-Oblivious/Emeralds"
-  url "https://github.com/Oblivious-Oblivious/Emeralds/archive/refs/tags/v0.10.10.tar.gz"
-  sha256 "e507211132495243f0fd6f99ce4553a751320b43da47e8c191e4b6ce9bd03177"
+  url "https://github.com/Oblivious-Oblivious/Emeralds/archive/refs/tags/v0.10.11.tar.gz"
+  sha256 "b39df9d855d977467ed74c7107f522b7c630cfca4bda0df5a7d8739f62d590ac"
   license "MIT"
 
   depends_on "crystal" => :build
