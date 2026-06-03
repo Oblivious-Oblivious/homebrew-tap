82ec1a262527ec52ca17d01eb36077065e99c42e
atha <atha@fourth-wall.org>
2026-06-04 01:20:26 +0300

    [master] - updated emeralds to 0.11.4

--- emeralds.rb
+++ emeralds.rb
@@ -1,8 +1,8 @@
 class Emeralds < Formula
   desc "A package manager for C"
   homepage "https://github.com/Oblivious-Oblivious/Emeralds"
-  url "https://github.com/Oblivious-Oblivious/Emeralds/archive/refs/tags/v0.11.3.tar.gz"
-  sha256 "dadc5e7467487b0ce20cfd427c42b19802eb07310768a271860cde4b754cd49c"
+  url "https://github.com/Oblivious-Oblivious/Emeralds/archive/refs/tags/v0.11.4.tar.gz"
+  sha256 "47e92f8470d2bb442f11d220c7fff9cb60cd719792c194bcbdfb2b6c3df78b0b"
   license "MIT"
 
   depends_on "crystal" => :build
